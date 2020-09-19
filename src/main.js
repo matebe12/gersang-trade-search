3cee020a36c9806dd0775d9b335bb99c7eba4c61
matebe12 <matebe12@gmail.com>
2020-09-20 05:02:15 +0900

    404

--- src/main.js
+++ src/main.js
@@ -9,11 +9,4 @@ new Vue({
   router,
   store,
   render: h => h(App),
-  created() {
-    if (sessionStorage.redirect) {
-      const redirect = sessionStorage.redirect;
-      delete sessionStorage.redirect;
-      this.$router.push(redirect);
-    }
-  },
 }).$mount('#app');
